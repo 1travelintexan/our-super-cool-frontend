bbbedec1d27e902d4a58d899f955a27d0dfcd62f
Joshua <horheyinc@gmail.com>
2024-12-18 14:56:37 +0100

    with credentials 2

--- src/pages/AddUserPage.jsx
+++ src/pages/AddUserPage.jsx
@@ -38,7 +38,7 @@ const AddUserPage = () => {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(newUser),
-        credentials: "include",
+        credentials: "same-origin",
       });
       const data = await response.json();
       console.log("here is the data", data);
